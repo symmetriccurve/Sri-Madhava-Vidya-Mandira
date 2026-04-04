fa4602abeae296c7133eedbcefd1174975509708
Vikram Belde <belvikram7@gmail.com>
2026-04-04 22:41:56 +0530

    bundle update
    add

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { createServer } from "./server";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/Sri-Madhava-Vidya-Mandira/' : '/',
+  base: '/',
   server: {
     host: "::",
     port: 8080,
